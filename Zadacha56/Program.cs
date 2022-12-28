065125a29ad38680bee47e0a927150364411ada1
AlFeraax <grigolin@mail.ru>
2022-12-28 20:03:21 +0300

    56 задача сделанa

--- Zadacha56/Program.cs
+++ Zadacha56/Program.cs
@@ -2,35 +2,35 @@
 
 void FillArray(int[,] array)
 {
-for (int i = 0; i < array.GetLength(0); i++)
-{
-for (int j = 0; j < array.GetLength(1); j++)
-{
-array[i, j] = new Random().Next(1, 11);
-}
-}
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 11);
+        }
+    }
 }
 
 void PrintArray(int[,] array)
 {
-for (int i = 0; i < array.GetLength(0); i++)
-{
-for (int j = 0; j < array.GetLength(1); j++)
-{
-Console.Write(array[i, j] + " ");
-}
-Console.WriteLine();
-}
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
 }
 
 int SumNumInLine(int[,] array, int i)
 {
-int sumLine = array[i, 0];
-for (int j = 1; j < array.GetLength(1); j++)
-{
-sumLine += array[i, j];
-}
-return sumLine;
+    int sumLine = array[i, 0];
+    for (int j = 1; j < array.GetLength(1); j++)
+    {
+        sumLine += array[i, j];
+    }
+    return sumLine;
 }
 
 int[,] array = new int[7, 3];
@@ -41,12 +41,12 @@ int minSumLine = 0;
 int sumLine = SumNumInLine(array, 0);
 for (int i = 1; i < array.GetLength(0); i++)
 {
-int tempSumLine = SumNumInLine(array, i);
-if (sumLine > tempSumLine)
-{
-sumLine = tempSumLine;
-minSumLine = i;
-}
+    int tempSumLine = SumNumInLine(array, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
 }
 Console.WriteLine();
 Console.WriteLine($"({minSumLine + 1}) - строкa с наименьшей суммой элементов, равной ({sumLine}).");
